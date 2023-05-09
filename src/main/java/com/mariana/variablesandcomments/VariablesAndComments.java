16b9f7e68483273cb00e1689a8b0f270ad689039
MarianaUserGit <maroleinic@gmail.com>
2023-05-09 18:14:08 +0300

    Added the Variables and Comments Task.

---
+++ src/main/java/com/mariana/variablesandcomments/VariablesAndComments.java
@@ -0,0 +1,47 @@
+package com.mariana.variablesandcomments;
+
+public class VariablesAndComments {
+    public static void main(String[] args) {
+
+//  Am declarat toate tipurile de variabile primitive
+
+        boolean falseBooleanVariable;
+        boolean trueBooleanVariable;
+        char firstCharFromAscii;
+        char lastCharFromAscii;
+        byte lowerLimitOfByteDataType;
+        byte upperLimitOfByteDataType;
+        short lowerLimitOfShortDataType;
+        short upperLimitOfShortDataType;
+        int lowerLimitOfIntDataType;
+        int upperLimitOfIntDataType;
+        long lowerLimitOfLongDataType;
+        long upperLimitOfLongDataType;
+        float lowerLimitOfFloatDataType;
+        float upperLimitOfFloatDataType;
+        double lowerLimitOfDoubleDataType;
+        double upperLimitOfDoubleDataType;
+
+//  Initializarea variabilelor declarate
+
+        falseBooleanVariable = false;
+        trueBooleanVariable = true;
+        firstCharFromAscii = 'a';
+        lastCharFromAscii = 'Z';
+        lowerLimitOfByteDataType = -128;
+        upperLimitOfByteDataType = 127;
+        lowerLimitOfShortDataType = -32768;
+        upperLimitOfShortDataType = 32767;
+        lowerLimitOfIntDataType = -2147483648;
+        upperLimitOfIntDataType = 2147483647;
+        lowerLimitOfLongDataType = -9223372036854775808L;
+        upperLimitOfLongDataType = 9223372036854775807L;
+        lowerLimitOfFloatDataType = -3.4E+36f;
+        upperLimitOfFloatDataType = 3.4E+36f;
+        lowerLimitOfDoubleDataType = -1.7E+308d;
+        upperLimitOfDoubleDataType = 1.7E+305;
+
+        System.out.println(firstCharFromAscii);
+
+    }
+}
