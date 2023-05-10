2d1ea823f36fda05332edd2c0416016d7e9a62b0
MarianaUserGit <maroleinic@gmail.com>
2023-05-10 20:17:41 +0300

    Added gitworkflow package and GitWorkflow class

---
+++ src/main/java/com/mariana/gitworkflow/GitWorkflow.java
@@ -0,0 +1,13 @@
+package com.mariana.gitworkflow;
+
+public class GitWorkflow {
+    public static void main(String[] args) {
+
+        String step1 = "Perform git stash - to store the uncommitted changes";
+        String step2 = "Modificam ultimul commit si facem git forced push";
+
+
+        System.out.println(step1);
+    }
+
+}
